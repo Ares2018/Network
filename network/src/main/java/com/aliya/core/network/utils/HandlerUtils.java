cbcaa4d228ad22fdc67f6a6299ce7da4ddb8ff7a
a_liYa <liyaf@8531.cn>
2017-12-27 14:22:33 +0800

    添加Handler工具类

--- network/src/main/java/com/aliya/core/network/utils/HandlerUtils.java
+++ network/src/main/java/com/aliya/core/network/utils/HandlerUtils.java
@@ -1,5 +1,8 @@
 package com.aliya.core.network.utils;
 
+import android.os.Handler;
+import android.os.Looper;
+
 /**
  * Handler 工具类
  *
@@ -7,5 +10,42 @@ package com.aliya.core.network.utils;
  * @date 2017/12/27 11:37.
  */
 public final class HandlerUtils {
+
+    private static volatile Handler sMainHandler;
+
+    /**
+     * 在主线程中执行runnable
+     *
+     * @param runnable 任务
+     */
+    public static void runInMainThread(Runnable runnable) {
+        if (runnable == null) return;
+        if (isRunInMainThread()) {
+            runnable.run();
+        } else {
+            getHandler().post(runnable);
+        }
+    }
+
+    /**
+     * 获取主线程的handler
+     */
+    public static Handler getHandler() {
+        if (sMainHandler == null) {
+            synchronized (HandlerUtils.class) {
+                if (sMainHandler == null) {
+                    sMainHandler = new Handler(Looper.getMainLooper());
+                }
+            }
+        }
+        return sMainHandler;
+    }
+
+    /**
+     * 判断当前的线程是不是在主线程
+     */
+    public static boolean isRunInMainThread() {
+        return Looper.getMainLooper().getThread() == Thread.currentThread();
+    }
 
 }
